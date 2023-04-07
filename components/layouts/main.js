d3e3481295d6e9168cc743ea09d6bb2ac9772ba2
Aakarsh B <aakarshbiju@gmail.com>
2023-04-07 14:25:54 +0530

    Update main.js

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -15,21 +15,21 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="Takuya's homepage" />
-        <meta name="author" content="Takuya Matsuyama" />
+        <meta name="description" content="aakarsh's homepage" />
+        <meta name="author" content="Aakarsh | Designer" />
         <meta name="author" content="craftzdog" />
         <link rel="apple-touch-icon" href="apple-touch-icon.png" />
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="twitter:title" content="Takuya Matsuyama" />
+        <meta name="twitter:title" content="Aakarsh B" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@craftzdog" />
-        <meta name="twitter:creator" content="@craftzdog" />
+        <meta name="twitter:site" content="@Aakarsh_B" />
+        <meta name="twitter:creator" content="@Aakarsh_B" />
         <meta name="twitter:image" content="https://www.craftz.dog/card.png" />
-        <meta property="og:site_name" content="Takuya Matsuyama" />
-        <meta name="og:title" content="Takuya Matsuyama" />
+        <meta property="og:site_name" content="aakarsh | designer" />
+        <meta name="og:title" content="aakarsh | designer" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="https://www.craftz.dog/card.png" />
-        <title>Takuya Matsuyama - Homepage</title>
+        <title>aakarsh | designer</title>
       </Head>
 
       <NavBar path={router.asPath} />
